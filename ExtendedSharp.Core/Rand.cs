e33b4e2da9af9ea675f3948e89a3aa02e5bdc19d
arman_arian <arman.hassanpoor2000@gmail.com>
2014-12-18 20:32:56 +0330

    Add Rand Class

---
+++ ExtendedSharp.Core/Rand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExtendedSharp.Core
+{
+    public static class Rand
+    {
+        public static double NextF(double minValue, double maxValue)
+        {
+            var random = new Random(Guid.NewGuid().GetHashCode());
+            return random.NextDouble() * (maxValue - minValue) + minValue;
+        }
+
+        public static int Next(int minValue, int maxValue)
+        {
+            var random = new Random(Guid.NewGuid().GetHashCode());
+            return random.Next(minValue, maxValue + 1);
+        }
+    }
+}
