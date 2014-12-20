9b0ab38f71d0d20436c00599aa385c8a1b82c102
arman_arian <arman.hassanpoor2000@gmail.com>
2014-12-20 10:05:00 +0330

    Add Extended Types

---
+++ ExtendedSharp.Core/ExtendedTypes/ExtendedBoolean.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExtendedSharp.Core.ExtendedTypes
+{
+    public static class ExBoolean
+    {
+        public static bool Not(this bool val)
+        {
+            return !val;
+        }
+
+        public static bool And(this bool valueA, bool valueB)
+        {
+            return valueA && valueB;
+        }
+
+        public static bool Or(this bool valueA, bool valueB)
+        {
+            return valueA && valueB;
+        }
+
+        public static int ToBinary(this bool b)
+        {
+            return b ? 1 : 0;
+        }
+
+        public static bool IfTrue(this bool b, Action action)
+        {
+            if (b)
+            {
+                action();
+            }
+            return b;
+        }
+
+        public static bool IfFalse(this bool b, Action action)
+        {
+            if (!b)
+            {
+                action();
+            }
+            return b;
+        }
+    }
+}
