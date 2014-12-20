73a82ee964746c91da240fd186667adce2aec817
arman_arian <arman.hassanpoor2000@gmail.com>
2014-12-20 10:28:29 +0330

    Add Extended String

--- ExtendedSharp.Core/ExtendedTypes/ExtendedString.cs
+++ ExtendedSharp.Core/ExtendedTypes/ExtendedString.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace ExtendedSharp.Core.ExtendedTypes
 {
     public static class ExtendedString
     {
+        public static string TrimAndReduce(this string str)
+        {
+            return ConvertWhitespacesToSingleSpaces(str).Trim();
+        }
+
+        public static string ConvertWhitespacesToSingleSpaces(this string value)
+        {
+            return Regex.Replace(value, @"\s+", " ");
+        }
     }
 }
