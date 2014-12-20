59695da123058d9ed60eceed1a203f5731ec526c
arman_arian <arman.hassanpoor2000@gmail.com>
2014-12-20 10:18:35 +0330

    Add Extened Methods

---
+++ ExtendedSharp.Core/ExtendedTypes/ExtendedEnumerable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ExtendedSharp.Core.ExtendedTypes
+{
+    public static class ExtendedEnumerable
+    {
+        public static string ToDelimString(this IEnumerable<string> arr, string delim = ",")
+        {
+            var ans = "";
+            var Enum = arr.GetEnumerator();
+            Enum.MoveNext();
+            while (Enum.Current != null)
+            {
+                ans += Enum.Current;
+                if (Enum.MoveNext())
+                    ans += delim;
+                else
+                    break;
+            }
+            return ans;
+        }
+    }
+}
